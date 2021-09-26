68ae537d0c8da95aed0409cf12b49b349c4c0bf3
LoweloDev <tobias-barthold@outlook.de>
2021-09-26 21:48:51 +0200

    gitignore not ignoring

--- Backend/src/main/java/com/studenthub/data/consts/GithubAuth.java
+++
@@ -1,6 +0,0 @@
-package com.studenthub.data.consts;
-
-public class GithubAuth {
-    public static final String username = "LoweloDev";
-    public static final String token = "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";
-}
