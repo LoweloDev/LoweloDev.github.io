aec7d62603fef98c362a33d96ff414b7cbd6c763
LoweloDev <tobias-barthold@outlook.de>
2022-08-14 23:02:00 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Sandbox/Playground(ReactJs & Spring Boot & Spring Data JPA + H2 Database & REST Api Endpoints)
-
-Frontend also fetches DB structure from Github just like the backend since it served as a React.js sandbox before backend even existed. Hence this is a disconnected mess and I was in the process of tidying it up and replicating backend functionality in the respective backend when a potentailly similar but expanded usecase came up, which is currently in private repo and will be done cleanly via Quarkus (probably even though I kinda like Springboot DI better semantically), RestEasy Reactive, Hibernate ORM Reactive, PostgreSQL Reactive and Websockets, together with a React.js frontend.
---
+++ precache-manifest.440f616eb99b2aa013eb4ffef74995eb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f183e5e12febadc3109b03deef2046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401556ee9bd8867e4b83",
+    "url": "/static/css/main.773ec729.chunk.css"
+  },
+  {
+    "revision": "db182acb543d590cecbf",
+    "url": "/static/js/2.a1741099.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a1741099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401556ee9bd8867e4b83",
+    "url": "/static/js/main.f46f89cf.chunk.js"
+  },
+  {
+    "revision": "c3c6eeac8edbb7669698",
+    "url": "/static/js/runtime-main.01c6a616.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "278054f622050d4a7c5f3931168da7d0",
+    "url": "/static/media/feature-tile-icon-01.278054f6.svg"
+  },
+  {
+    "revision": "0b92e6a493e6a9b1530629e9cf00dc7a",
+    "url": "/static/media/feature-tile-icon-02.0b92e6a4.svg"
+  },
+  {
+    "revision": "599c18bba3e97268866a451d58706b32",
+    "url": "/static/media/feature-tile-icon-03.599c18bb.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ed1bc89126431e84b24024580ade74bc",
+    "url": "/static/media/illustration-section-01.ed1bc891.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "f1d09cd19f573bad13431d1b4332de47",
+    "url": "/static/media/logo.f1d09cd1.svg"
+  },
+  {
+    "revision": "f52ea3d973c45aee58c598c25c3a00f7",
+    "url": "/static/media/vidplaceholder.f52ea3d9.png"
+  }
+]);
